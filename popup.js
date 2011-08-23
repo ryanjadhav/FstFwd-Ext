4b31fffdc91b822e44a7f81567e6c58000656882
Ryan Jadhav <ryan.jadhav@gmail.com>
2011-08-23 14:55:01 -0700

    Added Rdio support

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ Rdio:
 API Key: c6rv7hex6jacaeujwhyesd3k
 Shared Secret: KKDtQNQKBE
 */
+var bgPage = chrome.extension.getBackgroundPage();
 
 var submitHandler = function(){
 	var artist = $('#artist_input').val();
@@ -124,6 +125,76 @@ var lastfmLookup = function(url){
   //call other api's.
 }
 
+/*
+rdiofy.getUserPlaylist = function() {
+  var params = {
+    'method': 'POST',
+    'headers': {},
+    'parameters': {
+	},
+    'body':'&method=getPlaylists' 
+  };
+	console.log('Fetching user\'s playlist');
+ 	bgPage.oauth.sendSignedRequest(bgPage.DOCLIST_FEED, rdiofy.userPlaylistCallback, params);
+};
+
+bgPage.oauth.authorize(function() {
+	if (bgPage._rdioUser ==null) {
+		rdiofy.log('No user details cached...fetching', INFO);
+		rdiofy.getUserDetail();
+	}
+});
+*/
+
+var stringify = function(parameters) {
+  var params = [];
+  for(var p in parameters) {
+    params.push(p + '=' + parameters[p]);
+  }
+  return params.join('&');
+};
+
+var rdio_authorize = function(){
+	bgPage.oauth.authorize(rdioOnAuthorized);
+};
+
+var handleSuccess = function(resp, xhr) {
+	var data = JSON.parse(resp);	
+	console.log(data);
+	console.log(xhr);
+	
+	var rdio_url = data.result.results[0].shortUrl;
+	
+	$('.rdio_url').html(data.result.results[0].shortUrl);
+	
+	$('.rdio_url').html("<a href='javascript:chrome.tabs.create({\"url\":\"" + rdio_url +"\", \"selected\":true});window.close();'>"+ rdio_url +"</a>");
+};
+
+var rdioOnAuthorized = function(){
+
+	var artist = $('#artist_input').val();
+	var song = $('#song_input').val();
+	
+	var query = createQuery(artist, song);
+
+  var params2 = {
+  	'method' : 'search',
+		'query' : query,
+		'types' : 'Track',
+		'count' : '1'
+  };
+  
+  console.log(stringify(params2));
+  
+  var params = {	
+    'method': 'POST',
+    'body': stringify(params2)
+  };
+  
+  bgPage.oauth.sendSignedRequest(bgPage.DOCLIST_FEED, handleSuccess, params);
+};
+
 $(document).ready(function(){
 	$('#submit').click(submitHandler);
+	$('#rdio_auth').click(rdio_authorize);
 });
